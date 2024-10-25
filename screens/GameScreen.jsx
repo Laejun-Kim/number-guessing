65b7f60b0e2f48fcc1a3589292eea43d2d8682b4
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2024-10-25 17:04:47 +0900

    Refactor GameScreen component styles and structure

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 
 const GameScreen = () => {
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher of lower?</Text>
+      </View>
+      <View>Log Rounds</View>
     </View>
   );
 };
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 40,
+    alignItems: "center",
+  },
+});
